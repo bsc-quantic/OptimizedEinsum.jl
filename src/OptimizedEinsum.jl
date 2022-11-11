bf9f2085ee1809a98d3552acc2d194d6791c157f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-11 13:13:29 +0100

    Remove legacy exported symbols

--- src/OptimizedEinsum.jl
+++ src/OptimizedEinsum.jl
@@ -4,13 +4,10 @@ import Base: show, convert
 using Base.Iterators: flatten, repeated, take, drop
 using Random
 
-export largest_intermediate
-export contract, contract_path
-export rand_equation, get_symbol
-
 include("Counters.jl")
 include("Utils.jl")
 include("Optimizers/Optimizers.jl")
 
+export rand_equation, get_symbol
 
 end
