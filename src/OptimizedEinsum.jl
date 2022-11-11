dd7d8c1d9236395428d0d5aaf2d44c6be931d148
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-11 13:13:40 +0100

    Remove legacy imports

--- src/OptimizedEinsum.jl
+++ src/OptimizedEinsum.jl
@@ -1,9 +1,5 @@
 module OptimizedEinsum
 
-import Base: show, convert
-using Base.Iterators: flatten, repeated, take, drop
-using Random
-
 include("Counters.jl")
 include("Utils.jl")
 include("Optimizers/Optimizers.jl")
