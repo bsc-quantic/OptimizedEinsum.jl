31ce7405351d649d51cd15dccf3311c7fa36c20a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-11-14 16:34:36 +0100

    Add `ContractionPath` struct

---
+++ src/ContractionPath.jl
@@ -0,0 +1,36 @@
+using Base.Iterators: flatten
+
+struct ContractionPath
+    ssa_path::Vector{Vector{Int}}
+    inputs::Vector{Vector{Symbol}}
+    output::Vector{Symbol}
+    size::Dict{Symbol,Int}
+
+    function ContractionPath(path, inputs, output, size)
+        @assert issetequal(flatten([inputs..., output]), keys(size))
+
+        if pathtype(path) == :linear
+            path = linear_to_ssa(path)
+        end
+
+        new(path, inputs, output, size)
+    end
+end
+
+function signatures(path::ContractionPath)
+    inputs = copy(path.inputs)
+    for (i, j) ∈ path.ssa_path
+        push!(inputs, symdiff(inputs[i], inputs[j]) ∪ ∩(path.output, inputs[i], inputs[j]))
+    end
+
+    inputs
+end
+
+function flops(path::ContractionPath)
+    signs = signatures(path)
+    mapreduce(+, path.ssa_path) do (i, j)
+        a = signs[i]
+        b = signs[j]
+        flops(a, b, path.size, path.output)
+    end
+end
--- src/OptimizedEinsum.jl
+++ src/OptimizedEinsum.jl
@@ -2,10 +2,12 @@ module OptimizedEinsum
 
 include("Counters.jl")
 include("Utils.jl")
+include("ContractionPath.jl")
 include("Optimizers/Optimizers.jl")
 
+export flops, removedsize
 export rand_equation, get_symbol
-
+export ContractionPath
 import .Optimizers: optimize, Optimal, Greedy
 export optimize, Optimal, Greedy
 
