e948be0d2ae47289a383881353f1d9fa2a6ebbb1
Jofre Vallès <jofrevalles99@gmail.com>
2022-12-20 14:51:01 +0100

    Add Makie as a Require for Visualization.jl

--- src/OptimizedEinsum.jl
+++ src/OptimizedEinsum.jl
@@ -3,8 +3,12 @@ module OptimizedEinsum
 include("Counters.jl")
 include("Utils.jl")
 include("ContractionPath.jl")
-include("Visualization.jl")
 include("Solvers/Solvers.jl")
+
+using Requires: @require
+function __init__()
+    @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("Visualization.jl")
+end
 
 export flops, removedsize, rank
 export rand_equation, get_symbol
