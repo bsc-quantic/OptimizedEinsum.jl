3c769c4d4b31c7fe90578541c82e4f71bb3ae3ac
Jofre Vallès <jofrevalles99@gmail.com>
2023-01-16 16:00:16 +0100

    Remove seed and add noise to cost_fn

--- src/Solvers/Random.jl
+++ src/Solvers/Random.jl
@@ -1,6 +1,7 @@
 using OptimizedEinsum: removedsize, ssa_path_cost, ContractionPath
 using OptimizedEinsum.Solvers: greedy_choose_simple!, greedy_choose_thermal!
 using Base: @kwdef
+using Distributions: truncated, Normal
 using Random: seed!
 
 abstract type RandomSolver <: Solver end
@@ -19,12 +20,12 @@ function contractpath(config::RandomGreedy, inputs, output, size)
     best = Dict{String,Any}("flops" => Inf, "size" => Inf)
 
     choose_fn = (queue, remaining) -> config.choose_fn(queue, remaining; nbranch=config.nbranch, temperature=config.temperature, rel_temperature=config.rel_temperature)
+    cost_fn = (a, b, size, keep) -> rand(truncated(Normal(1.0, 0.01),0,10),1)[1]*removedsize(a, b, size, keep)
 
-    trials = [trial_greedy_ssa_path_and_cost(i, inputs, output, size, choose_fn, config.cost_fn) for i in 1:config.repeats]
+    trials = [trial_greedy_ssa_path_and_cost(i, inputs, output, size, choose_fn, cost_fn) for i in 1:config.repeats]
 
     # assess the trials
     for (ssa_path, cost, size) in trials
-
         if is_better(config.minimize, cost, size, best["flops"], best["size"])
             best["flops"] = cost
             best["size"] = size
@@ -36,8 +37,6 @@ function contractpath(config::RandomGreedy, inputs, output, size)
 end
 
 function trial_greedy_ssa_path_and_cost(r, inputs, output, size_dict, choose_fn, cost_fn)
-    seed!(r)
-
     ssa_path = ssa_greedy_optimize(inputs, output, size_dict, choose_fn, cost_fn)
     flops, size = ssa_path_cost(ssa_path, inputs, output, size_dict)
 
