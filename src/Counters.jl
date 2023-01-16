14bb57167fbaa10d4f21401005ab6711e65a9a16
Jofre Vallès Muns <61060572+jofrevalles@users.noreply.github.com>
2023-01-16 16:26:41 +0100

    Apply @mofeing suggestions from code review
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>

--- src/Counters.jl
+++ src/Counters.jl
@@ -6,4 +6,4 @@ flops(a, b, size, keep) = prod(ind -> size[ind], flatten((a ∪ b, ∩(keep, a, 
 
 rank(a, b, size, keep) = length(symdiff(a, b) ∪ ∩(keep, a, b))
 
-removedsize(a, b, size, keep) = prod(size[ind] for ind ∈ symdiff(a, b) ∪ (keep ∩ a) ∪ (keep ∩ b); init=Float64(1)) - prod(size[ind] for ind ∈ a) - prod(size[ind] for ind ∈ b)
+removedsize(a, b, size, keep) = prod(size[ind] for ind ∈ symdiff(a, b) ∪ (keep ∩ a) ∪ (keep ∩ b); init=1) - prod(size[ind] for ind ∈ a) - prod(size[ind] for ind ∈ b)
