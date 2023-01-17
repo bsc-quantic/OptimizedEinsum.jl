ea8a02cd36fbf7ed05327c1842ca36d5c5cb11b9
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-17 20:02:06 +0100

    Fix attribute overriding on plotting
    Closes #22

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -11,20 +11,23 @@ function Makie.plot!(P::GraphPlot{Tuple{ContractionPath}}; kwargs...)
     graph = SimpleDiGraph([Edge(from, to) for (pair, to) in zip(path.ssa_path, Iterators.countfrom(length(path.inputs) + 1)) for from in pair])
 
     kwargs = Dict{Symbol,Any}(kwargs)
-    get!(kwargs, :edge_width) do
-        [log10(size(path, i)) for i in 1:ne(graph)]
+    scene = Makie.parent_scene(P)
+    default_attrs = default_theme(scene, GraphPlot)
+
+    if P.attributes.edge_width[] == default_attrs.edge_width[]
+        kwargs[:edge_width] = [log10(size(path, i)) for i in 1:ne(graph)]
     end
 
-    get!(kwargs, :arrow_size) do
-        [3 * log10(size(path, i)) for i in 1:ne(graph)]
+    if P.attributes.arrow_size[] == default_attrs.arrow_size[]
+        kwargs[:arrow_size] = [3 * log10(size(path, i)) for i in 1:ne(graph)]
     end
 
-    get!(kwargs, :edge_color) do
-        [log10(size(path, i)) for i in 1:ne(graph)]
+    if P.attributes.edge_color[] == default_attrs.edge_color[]
+        kwargs[:edge_color] = [log10(size(path, i)) for i in 1:ne(graph)]
     end
 
-    get!(kwargs, :node_size) do
-        [(x = log2(flops(path, i)); isinf(x) ? DEFAULT_LEAF_NODE_SIZE : x) for i in 1:nv(graph)]
+    if P.attributes.node_size[] == default_attrs.node_size[]
+        kwargs[:node_size] = [(x = log2(flops(path, i)); isinf(x) ? DEFAULT_LEAF_NODE_SIZE : x) for i in 1:nv(graph)]
     end
 
     graphplot!(P, graph; kwargs...)
