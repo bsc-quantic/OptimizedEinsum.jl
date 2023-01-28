e3eaae8ffcac7ee4652ed70cdf9a6a7f3b5073da
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2023-01-28 14:03:23 +0100

    Group `Aqua` tests into "Aqua" testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 import OptimizedEinsum
 
-using Aqua
-Aqua.test_all(OptimizedEinsum, ambiguities=false, stale_deps=false)
-Aqua.test_ambiguities(OptimizedEinsum)
+@testset "Aqua" verbose = true begin
+    using Aqua
+    Aqua.test_all(OptimizedEinsum, ambiguities=false, stale_deps=false)
+    Aqua.test_ambiguities(OptimizedEinsum)
+end
