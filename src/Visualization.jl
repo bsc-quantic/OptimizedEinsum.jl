818f7dbd7f2502bca1f424e8ed5a3b77c477a411
Jofre Vallès Muns <61060572+jofrevalles@users.noreply.github.com>
2023-02-07 18:23:28 +0100

    Refactor visualization methods (#28)
    * Refactor visualization function
    
    * Fix code from last commit
    
    * Refactor plot functions
    
    * Remove package from Project.toml
    
    * Apply @mofeing suggestions from code review
    
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
    
    * Apply @mofeing suggestions from code review
    
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
    
    * Apply @mofeing suggestions from code review
    
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
    
    * Final fixes and combine code
    
    * Add TODO comment
    
    * Add missing import
    
    * Combine code expressions
    
    ---------
    
    Co-authored-by: Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -1,34 +1,72 @@
 using Graphs
 using Makie
 using GraphMakie
+using GraphMakie.NetworkLayout
 
-const DEFAULT_LEAF_NODE_SIZE = 5.0
+# TODO rework size calculating algorithm
+const MIN_LOGFLOP = 0.01
+const MAX_EDGE_WIDTH = 10.
+const MAX_ARROW_SIZE = 25.
+const MAX_NODE_SIZE = 40.
 
-Makie.plottype(::ContractionPath) = GraphPlot
+function Makie.plot(path::ContractionPath; colormap = to_colormap(:viridis)[begin:end-10], kwargs...)
+    f = Figure()
 
-function Makie.plot!(P::GraphPlot{Tuple{ContractionPath}}; kwargs...)
-    path = P[1][]
-    graph = SimpleDiGraph([Edge(from, to) for (pair, to) in zip(path.ssa_path, Iterators.countfrom(length(path.inputs) + 1)) for from in pair])
+    p, ax = plot!(f[1,1], path; colormap, kwargs...)
+    display(f)
 
-    kwargs = Dict{Symbol,Any}(kwargs)
-    scene = Makie.parent_scene(P)
+    return f, ax, p
+end
+
+# TODO replace `to_colormap(:viridis)[begin:end-10]` with a custom colormap
+function Makie.plot!(f::GridPosition, path::ContractionPath; colormap = to_colormap(:viridis)[begin:end-10], kwargs...)
+    scene = Scene()
     default_attrs = default_theme(scene, GraphPlot)
 
-    if P.attributes.edge_width[] == default_attrs.edge_width[]
-        kwargs[:edge_width] = [log10(size(path, i)) for i in 1:ne(graph)]
+    kwargs = Dict{Symbol,Any}(kwargs)
+
+    graph = SimpleDiGraph([Edge(from, to) for (pair, to) in zip(path.ssa_path, Iterators.countfrom(length(path.inputs) + 1)) for from in pair])
+
+    log_size = [log2(size(path, i)) for i in 1:ne(graph)]
+    log_flop = [(x = log10(flops(path, i)); isinf(x) ? MIN_LOGFLOP : x) for i in 1:nv(graph)]
+
+    min_size, max_size = extrema(log_size)
+    min_flop, max_flop = extrema(log_flop)
+
+    kwargs[:edge_width] = (log_size/max_size) * MAX_EDGE_WIDTH
+    kwargs[:arrow_size] = (log_size/max_size) * MAX_ARROW_SIZE
+    kwargs[:edge_color] = log_size
+    kwargs[:node_size] = (log_flop/max_flop) * MAX_NODE_SIZE
+    kwargs[:node_color] = log_flop
+
+    if haskey(kwargs, :layout) && kwargs[:layout] isa GraphMakie.NetworkLayout.IterativeLayout{3}
+        ax = LScene(f[1,1])
+    else
+        ax = Axis(f[1,1])
+        # hide decorations if it is not a 3D plot
+        hidedecorations!(ax)
+        hidespines!(ax)
+        ax.aspect = DataAspect()
     end
 
-    if P.attributes.arrow_size[] == default_attrs.arrow_size[]
-        kwargs[:arrow_size] = [3 * log10(size(path, i)) for i in 1:ne(graph)]
-    end
+    p = graphplot!(f[1,1], graph;
+        arrow_attr = (colorrange=(min_size, max_size), colormap=colormap),
+        edge_attr = (colorrange=(min_size, max_size), colormap=colormap),
+        node_attr = (colorrange=(min_flop, max_flop),
+        # TODO replace `to_colormap(:plasma)[begin:end-50]), kwargs...)` with a custom colormap
+        colormap = to_colormap(:plasma)[begin:end-50]),
+        kwargs...)
 
-    if P.attributes.edge_color[] == default_attrs.edge_color[]
-        kwargs[:edge_color] = [log10(size(path, i)) for i in 1:ne(graph)]
-    end
+    # TODO configurable `labelsize`
+    cbar = Colorbar(f[1,2], get_edge_plot(p), label = L"\log_{2}(size)", flip_vertical_label = true, labelsize = 34)
+    cbar.height = Relative(5/6)
 
-    if P.attributes.node_size[] == default_attrs.node_size[]
-        kwargs[:node_size] = [(x = log2(flops(path, i)); isinf(x) ? DEFAULT_LEAF_NODE_SIZE : x) for i in 1:nv(graph)]
-    end
+    cbar2 = Colorbar(f[1,0], get_node_plot(p), label = L"\log_{10}(flops)", flipaxis = false, labelsize = 34)
+    cbar2.height = Relative(5/6)
 
-    graphplot!(P, graph; kwargs...)
+    # TODO configurable alignments
+    cbar2.alignmode = Mixed(left = -10, right = -30)
+    cbar.alignmode = Mixed(left = -30, right = -10)
+
+    return p, ax
 end
