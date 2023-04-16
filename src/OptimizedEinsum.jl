1bbe8dd1a5aa49110ac090e97b635b1e36f4ce11
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2023-04-17 01:16:28 +0200

    Update deprecation notice

--- src/OptimizedEinsum.jl
+++ src/OptimizedEinsum.jl
@@ -9,10 +9,12 @@ include("Solvers/Solvers.jl")
 function __init__()
     @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("Visualization.jl")
     @warn """This is the last release of `OptimizedEinsum` on the General official registry.
-        Further development can be found in our registry https://github.com/bsc-quantic/Registry in the form of `OptimizedEinsum` or subdivided packages.
-        If you want to use our registry just do:
+        It has been replaced by `EinExprs` (https://github.com/bsc-quantic/EinExprs.jl` which can be found in our registry https://github.com/bsc-quantic/Registry in the form of `OptimizedEinsum` or subdivided packages.
+        If you want to use `EinExprs`, add our registry:
         \tusing Pkg
         \tpkg"registry add https://github.com/bsc-quantic/Registry"
+        And then,
+        \tpkg"add EinExprs"
     """
 end
 
